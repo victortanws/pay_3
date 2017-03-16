c1f40ca2de483a42bb81a301a223d5a80a1a2aff
Raghu Betina <raghu@firstdraft.com>
2017-03-16 02:07:46 +0000

    Automatically associate comment to signed in user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -47,8 +47,6 @@ class LikesController < ApplicationController
 
   def update
     @like = Like.find(params[:id])
-
-    @like.user_id = params[:user_id]
     @like.payment_id = params[:payment_id]
 
     save_status = @like.save
